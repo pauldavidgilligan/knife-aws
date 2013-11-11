2be36d4af29ccd21fe7ecbc547250b0de2633001
Paul Gilligan <pauldavidgilligan@mac.com>
2013-11-11 17:38:28 +0000

    basic knife setup

--- knife-aws.gemspec
+++ knife-aws.gemspec
@@ -11,12 +11,13 @@ Gem::Specification.new do |gem|
   gem.license       = 'Apache 2.0'
   gem.summary       = %q{Extends knife to manage aws applications}
   gem.description   = %q{A utility to manage aws applications end to end}
-  gem.homepage      = ""
+  gem.homepage      = "https://github.com/pauldavidgilligan/knife-aws"
 
   gem.files         = `git ls-files`.split($/)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.add_dependency 'safe_yaml'
+  gem.add_dependency 'fog'
+  gem.add_dependency 'rb-readline'
   gem.add_development_dependency 'chef'
   gem.require_paths = ["lib"]
 end
--- lib/chef/knife/knife-aws.rb
+++ lib/chef/knife/knife-aws.rb
@@ -3,6 +3,7 @@ require 'chef/knife'
 class Chef
   class Knife
     module AwsBase
+
       def self.included(includer)
         includer.class_eval do
 
@@ -11,6 +12,11 @@ class Chef
             require 'readline'
             require 'chef/json_compat'
           end
+
+          option :aws_credential_file,
+            :long => "--aws-credential-file FILE",
+            :description => "File containing AWS credentials as used by aws cmdline tools",
+            :proc => Proc.new { |key| Chef::Config[:knife][:aws_credential_file] = key }
 
           option :aws_access_key_id,
             :short => "-A ID",
@@ -23,8 +29,59 @@ class Chef
             :long => "--aws-secret-access-key SECRET",
             :description => "Your AWS API Secret Access Key",
             :proc => Proc.new { |key| Chef::Config[:knife][:aws_secret_access_key] = key }
-          
+
+          option :region,
+            :long => "--region REGION",
+            :description => "Your AWS region",
+            :proc => Proc.new { |key| Chef::Config[:knife][:region] = key }
+
+          option :confluence_config_url,
+            :short => '-C URL',
+            :long => '--confluence-config-url URL',
+            :description => "Specify confluence rest api url",
+            :proc => Proc.new { |url| Chef::Config[:knife][:confluence_config_url] = url }
+
         end
       end
+
+      def exit_with_message(message)
+        ui.fatal message
+        Kernel.exit(1)
+      end
+
+      def connection
+        @connection ||= begin
+          connection = Fog::Compute.new(
+            :provider => 'AWS',
+            :aws_access_key_id => Chef::Config[:knife][:aws_access_key_id],
+            :aws_secret_access_key => Chef::Config[:knife][:aws_secret_access_key],
+            :region => locate_config_value(:region)
+          )
+        end
+      end
+
+      def locate_config_value(key)
+        key = key.to_sym
+        config[key] || Chef::Config[:knife][key]
+      end
+
+      def validate!
+        if Chef::Config[:knife][:confluence_config_url].nil?
+          exit_with_message("Confluence wiki URL parameter is required.")
+        end
+      end
+
+    end
   end
+
+  class AwsSecurityList < Chef::Knife
+    include Knife::AwsBase
+    banner "knife aws security list"
+
+    def run
+      validate!
+    end
+  end
+
+
 end
